c48b9e2dbb3df3f3e3b35951c1941eae028e9e34
Vineet Kumar <yadavvineet07@gmail.com>
2022-07-07 22:34:15 +0530

    tree series day 1

---
+++ inorderTraversal.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull long long
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define vec vector
+
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+vector < int > inOrderTrav(node * curr) {
+  vector < int > inOrder;
+  stack < node * > s;
+  while (true) {
+    if (curr != NULL) {
+      s.push(curr);
+      curr = curr -> left;
+    } else {
+      if (s.empty()) break;
+      curr = s.top();
+      inOrder.push_back(curr -> data);
+      s.pop();
+      curr = curr -> right;
+    }
+  }
+  return inOrder;
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin); 
+		freopen("output.txt","w",stdout); 
+	#endif
+
+	//mahadev
+	  struct node * root = newNode(1);
+	  root -> left = newNode(2);
+	  root -> right = newNode(3);
+	  root -> left -> left = newNode(4);
+	  root -> left -> right = newNode(5);
+	  root -> left -> right -> left = newNode(8);
+	  root -> right -> left = newNode(6);
+	  root -> right -> right = newNode(7);
+	  root -> right -> right -> left = newNode(9);
+	  root -> right -> right -> right = newNode(10);
+
+	  vector < int > inOrder;
+	  inOrder = inOrderTrav(root);
+
+	  cout << "The inOrder Traversal is : ";
+	  for (int i = 0; i < inOrder.size(); i++) {
+	    cout << inOrder[i] << " ";
+	  }
+	
+	return 0;
+}
